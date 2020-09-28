728d9a1a1847d5704267eeae3bf0e6e8dc78d458
Davi <davicpinheiro123@hotmail.com>
2020-09-28 17:55:42 -0300

    umbler

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
+
 consign()
     .include('./src/database/connection.js')
     .then('./src/database/models')
