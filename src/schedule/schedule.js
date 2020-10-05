54ed25d347e439574b724bfdba2e44f64589cc20
Davi <davicpinheiro123@hotmail.com>
2020-10-05 15:15:31 -0300

    Fixing deploy issue

--- src/schedule/schedule.js
+++ src/schedule/schedule.js
@@ -69,7 +69,12 @@ module.exports = async app => {
     }
 
     async function mangaYabuScrap() {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            'args': [
+                '--no-sandbox',
+                '--disable-setuid-sandbox'
+            ]
+        });
         const page = await browser.newPage();
         // await page.emulate(iPhone);
         await page.goto('https://mangayabu.com/manga/vagabond/');
